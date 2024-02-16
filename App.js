1fe7591e0f8239713c800d94ad7f8c72892b8c40
GyovanneMS <gyovannecontato@gmail.com>
2024-02-16 15:14:39 -0300

    Starting

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, StatusBar, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <ScrollView>
+      <StatusBar backgroundColor="#f54748" barStyle="light-content" />
+      <View style={styles.topo}>
+        <Image style={styles.topoImagem}  source={ require("./assets/images/icon.png")} />
+
+        <Text style={ styles.topoTexto}> Melograno {"\n"} Cozinhando com APPs</Text>
+      </View>
+      <View>
+        <Text> Pudim </Text>
+      </View>
+    </ScrollView>
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    tela:{
+        backgroundColor: "#fff"
+    },
+    topo: {
+        alignItems: "center",
+        backgroundColor:  "#f54748",
+        padding:32
+    },
+    topoImagem: {
+        height: 64,
+        width: 64
+    },
+    topoTexto: {
+        color: "#fff",
+        fontSize: 32,
+        textAlign: "center"
+    }
 });
