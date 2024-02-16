dfee95f06b3241650c59f97ca54e04c44283caca
GyovanneMS <gyovannecontato@gmail.com>
2024-02-16 15:30:59 -0300

    Style

--- App.js
+++ App.js
@@ -9,8 +9,44 @@ export default function App() {
 
         <Text style={ styles.topoTexto}> Melograno {"\n"} Cozinhando com APPs</Text>
       </View>
-      <View>
-        <Text> Pudim </Text>
+      <View style={styles.receita}>
+        <Text style={styles.receitaTitulo}> Pudim </Text>
+
+        <Image style={styles.receitaImagem} source={require("./assets/images/Pudim.jpg")}/>
+
+        <Text style={styles.receitaSubtitulo}>Ingredientes:</Text>
+        <Text>
+        1 lata de leite condensado
+        leite {"\n"}
+        1 lata de leite (medida da lata de leite condensado)
+        ovo {"\n"}
+        3 ovos inteiros {"\n"}
+        1 lata de leite condensado
+        leite {"\n"}
+        1 lata de leite (medida da lata de leite condensado)
+        ovo {"\n"}
+        3 ovos inteiros
+        </Text>
+
+        <Text style={styles.receitaSubtitulo}>Modo de preparo:</Text>
+        <Text>
+        1{"\n"}
+        Pudim{"\n"}
+        Primeiro, bata bem os ovos no liquidificador.{"\n"}
+        2{"\n"}
+        Acrescente o leite condensado e o leite, e bata novamente.{"\n"}
+        3{"\n"}
+        Calda{"\n"}
+        Derreta o açúcar na panela até ficar moreno, acrescente a água e deixe engrossar.{"\n"}
+        4{"\n"}
+        Coloque em uma forma redonda e despeje a massa do pudim por cima.{"\n"}
+        5{"\n"}
+        Asse em forno médio por 45 minutos, com a assadeira redonda dentro de uma maior com água.{"\n"}
+        6{"\n"}
+        Espete um garfo para ver se está bem assado.{"\n"}
+        7{"\n"}
+        Deixe esfriar e desenforme.
+        </Text>
       </View>
     </ScrollView>
 
@@ -33,5 +69,23 @@ const styles = StyleSheet.create({
         color: "#fff",
         fontSize: 32,
         textAlign: "center"
+    },
+    receita:{
+        marginVertical: 16,
+        marginHorizontal: 32
+    },
+    receitaImagem: {
+        height: 200,
+        width: "auto"
+    },
+    receitaTitulo: {
+        fontSize: 32,
+        fontWeight: "bold",
+        marginVertical: 16,
+        textAlign: "center"
+    },
+    receitaSubtitulo: {
+        fontSize: 24,
+        margin: 16
     }
 });
