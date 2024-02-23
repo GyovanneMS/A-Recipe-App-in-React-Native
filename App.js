b130d58aec0155300b18d34f747e3c9a552c95ae
GyovanneMS <gyovannecontato@gmail.com>
2024-02-23 14:14:10 -0300

    Finish

--- App.js
+++ App.js
@@ -47,6 +47,60 @@ export default function App() {
         7{"\n"}
         Deixe esfriar e desenforme.
         </Text>
+        <Text style={styles.receitaTitulo}> Bolo pudim </Text>
+
+        <Image style={styles.receitaImagem} source={require("./assets/images/Pudilo.jpg")}/>
+
+        <Text style={styles.receitaSubtitulo}>Ingredientes:</Text>
+        <Text>
+        <Text style={styles.receitaSubtitulo}>Bolo</Text>{"\n"}
+        ovo{"\n"}
+        3 ovos{"\n"}
+        farinha de trigo{"\n"}
+        3 xícaras farinha de trigo{"\n"}
+        açúcar{"\n"}
+        2 xícaras de açúcar{"\n"}
+        margarina{"\n"}
+        4 colheres de margarina{"\n"}
+        leite{"\n"}
+        1 xícara de leite{"\n"}
+        sal{"\n"}
+        1 pitada de sal{"\n"}
+        fermento{"\n"}
+        3 colherzinhas de fermento{"\n"}
+        <Text style={styles.receitaSubtitulo}>Pudim</Text>{"\n"}
+        Pudim{"\n"}
+        ovo{"\n"}
+        3 ovos{"\n"}
+        leite condensado{"\n"}
+        1 lata de leite condensado{"\n"}
+        leite{"\n"}
+        leite (a mesma medida da lata de leite condensado){"\n"}
+
+        </Text>
+
+        <Text style={styles.receitaSubtitulo}>Modo de preparo:</Text>
+
+        <Text> 
+        1h{"\n"}
+        1{"\n"}
+        Bata a manteiga com o açúcar e os ovos, por 5 minutos.{"\n"}
+
+        2{"\n"}
+        Depois coloque a farinha e o leite, o sal e por último o fermento, continue batendo.{"\n"}
+
+        3{"\n"}
+        Caramelize a forma e coloque a massa.{"\n"}
+
+        4{"\n"}
+        Bata no liquidificador os ingredientes do pudim.{"\n"}
+
+        5{"\n"}
+        Coloque em cima da massa.{"\n"}
+
+        6{"\n"}
+        Leve ao forno médio, preaquecido, por aproximadamente 30 minutos.{"\n"}
+        </Text>
       </View>
     </ScrollView>
 
